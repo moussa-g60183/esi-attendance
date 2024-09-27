cef157e669a2fcb2750c7449d5809bf5183b2250
Gilles Chalon <60534@etu.he2b.be>
2024-09-27 10:51:52 +0200

    Modification mise en page attendande
    Modification des classes tailwindcss de certains éléments de la page principale pour améliorer le style

--- attendance-app/resources/views/livewire/attendance-page.blade.php
+++ attendance-app/resources/views/livewire/attendance-page.blade.php
@@ -3,10 +3,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pikaday/css/pikaday.css">
 @endassets
 
-<div class="w-3/4 mx-auto py-4 px-4 rounded-md shadow-sm bg-white">
+<div class="w-3/4 px-4 py-4 mx-auto bg-white rounded-md shadow-sm">
     <div class="py-4">
         <select wire:model.change="selectedValue" name="course" id="course"
-            class="py-3 px-4 pe-9 block w-auto mx-auto bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:focus:ring-neutral-600">
+            class="block w-auto px-4 py-3 mx-auto text-sm bg-gray-100 border-transparent rounded-lg pe-9 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none ">
             <option value="">-- Sélectionner un cours --</option>
             @foreach ($courses as $course)
                 <option value="{{ $course['sigle'] }}">{{ $course['sigle'] }}</option>
@@ -15,20 +15,20 @@
     </div>
     <div class="py-4">
         @if ($data)
-            <table class="min-w-full text-left text-sm font-light text-surface">
-                <thead class="border-b border-neutral-200 font-medium dark:border-white/10">
+            <table class="min-w-full text-sm font-light text-left text-surface">
+                <thead class="font-medium border-b border-neutral-200 dark:border-white/10">
                     <tr>
-                        <th class="px-6 p-4">Matricule</th>
-                        <th class="px-6 p-4">Prénom</th>
-                        <th class="px-6 p-4">Nom</td>
+                        <th class="p-4 px-6">Matricule</th>
+                        <th class="p-4 px-6">Prénom</th>
+                        <th class="p-4 px-6">Nom</td>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($data as $item)
                         <tr class="border-b border-neutral-200 dark:border-white/10">
-                            <td class="whitespace-nowrap px-6 py-4">{{ $item['matricule'] }}</td>
-                            <td class="whitespace-nowrap px-6 py-4">{{ $item['first_name'] }}</td>
-                            <td class="whitespace-nowrap px-6 py-4">{{ $item['last_name'] }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $item['matricule'] }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $item['first_name'] }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $item['last_name'] }}</td>
                         </tr>
                     @endforeach
                 </tbody>
